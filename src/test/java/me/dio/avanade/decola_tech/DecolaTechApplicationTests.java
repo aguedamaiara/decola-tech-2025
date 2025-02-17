ad525d7dd18f871caf671e018dc7d83f0d606cbc
aguedamaiara <costaaguedama@gmail.com>
2025-02-17 08:29:58 -0300

    1o commit

---
+++ src/test/java/me/dio/avanade/decola_tech/DecolaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.avanade.decola_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
